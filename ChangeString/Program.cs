fd63791b9af28f2f275ba8a918421b8602c976f0
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-03-06 14:01:08 +0000

    add changestring

---
+++ ChangeString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Inputs pedidos no ínicio
+            Console.Write("Write a phrase: ");
+            string phrase = Console.ReadLine();
+            Console.Write("Write a character: ");
+            string character = Console.ReadLine();
+
+            //Conversão da letra para Char
+            char character_to_char = char.Parse(character);
+
+            //Substituição e recolocação
+            string modified = phrase.Replace(character_to_char,'x');
+            Console.WriteLine(modified);
+        }
+    }
+}
